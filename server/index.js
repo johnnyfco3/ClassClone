10239f432046a9f325942064c5f67b8e5f56154d
johnnytejada <johnnyfran20002@gmail.com>
2022-03-31 14:34:37 -0400

    using dotenv

--- server/index.js
+++ server/index.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const express = require('express')
 const usersController = require('./controllers/users')
 const app = express()
@@ -11,7 +12,7 @@ app
   .use(express.json())
   
   .get('/api/', (req, res) => {
-    res.send('You are on the homepage')
+    res.send('You are at the root of the API for the best class ever - ' + process.env.BEST_CLASS_EVER)
   })
   .use('/api/users', usersController)
 
