496da5b496a8e3a13ed72c979117574995f79202
johnnyfco3 <72476506+johnnyfco3@users.noreply.github.com>
2022-04-04 19:20:10 +0000

    Deploying to node-deploy from @ johnnyfco3/ClassClone@d2efe9115812e1f97362236f51bed67446bdc716 🚀

--- server/controllers/users.js
+++ server/controllers/users.js
@@ -1,11 +1,13 @@
 const express = require("express")
 const app = express.Router()
+const { requireAuth } = require("../models/auth")
+
 const userModel = require('../models/user')
 
 const CREATED_STATUS = 201
 
 app
-    .get('/', (req,res) =>{
+    .get('/', requireAuth, (req,res) =>{
         res.send(userModel.list)
     })
     .get('/:id', (req,res)=>{
@@ -18,7 +20,7 @@ app
             res.status(CREATED_STATUS).send(user)
         }).catch(next)
     })
-    .delete('/:id', (req,res)=>{
+    .delete('/:id', requireAuth, (req,res)=>{
         const user = userModel.remove(req.params.id)
         res.send({ success: true, errors: [], date: user })
     })
@@ -26,6 +28,12 @@ app
         userModel.update(req.params.id, req.body)
         .then(user => {
             res.send({ success: true, errors: [], date: user })
+        }).catch(next)
+    })
+    .post('/login', (req,res, next)=>{
+        userModel.login(req.body.email, req.body.password)
+        .then(user => {
+            res.send(user)
         }).catch(next)
     })
 
--- server/models/post.js
+++ server/models/post.js
@@ -9,57 +9,66 @@ const list = [
         owner: 1,
         likes: [],
         comments: [],
-        isPublic: true
+        isPublic: true,
+        id: 1
     },
     {
-        src: "https://media-cldnry.s-nbcnews.com/image/upload/t_focal-560x280,f_auto,q_auto:best/newscms/2022_13/3543963/220328-ukriane-mb-0729.jpg",
-        caption: "Russian forces focus on eastern Ukraine amid rising fears they may seek to split the country in two",
+        src: 'https://w2.chabad.org/media/images/1187/LUSK11877068.jpg?_i=_nF72F3FCB89E9790A548A28674376BE69',
+        caption: 'How Jews in Ukraine Will Celebrate Passover This Year',
         owner: 1,
         likes: [],
         comments: [],
-        isPublic: true
+        isPublic: true,
+        id: 2
     },
     {
-        src: "https://media-cldnry.s-nbcnews.com/image/upload/t_focal-560x280,f_auto,q_auto:best/newscms/2022_13/3543963/220328-ukriane-mb-0729.jpg",
-        caption: "Russian forces focus on eastern Ukraine amid rising fears they may seek to split the country in two",
-        owner: 1,
+        src: 'https://w2.chabad.org/media/images/1187/sTyy11875354.jpg?_i=_nF72F3FCB89E9790A548A28674376BE69',
+        caption: 'What It Has Been Like to Host Ukraine Refugees',
+        owner: 2,
         likes: [],
         comments: [],
-        isPublic: true
+        isPublic: true,
+        id: 3
     },
     
 ]
 
+const includeUser = post => ({ ...post, user: userModel.get(post.owner) });
+
 function get(id){
-    return { ...list.find(x => x.id === parseInt(id)), user: userModel.get(x.owner) }
+    const post = list.find(x => x.id === parseInt(id));
+    if(!post){
+        throw { status: 404, message: 'Post not found' };
+    }
+    return includeUser(post);
 }
 
 function remove(id){
     const index = list.findIndex(x => x.id === parseInt(id))
     const post = list.splice(index, 1)
 
-    return { ...post[0], user: userModel.get(post[0].owner) }
+    return includeUser(post[0]);
 }
 
 function update(id, newPost){
     const index = list.findIndex(x => x.id === parseInt(id))
     const oldPost = list[index]
     
-    newPost = { ...oldPost, ...newPost }
+    newPost = list[index] = { ...oldPost, ...newPost }
 
-    return { ...newPost, user: userModel.get(x.owner) }
+    return includeUser(newPost)
 }
 
 module.exports = {
     create(post) {
         post.id = ++highestId
         list.push(post)
-        return { ...post, user: userModel.get(x.owner) }
+        return  includeUser(post);
     },
     remove,
     update,
     get list(){
-        return list.map(x => ({ ...x, user: userModel.get(x.owner) }))
+        return list.map(x=> includeUser(x) );
     }
 }
 
