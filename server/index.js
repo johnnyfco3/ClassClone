755c9c96fbe231986ef0eb677ad68bdbd02e2629
johnnytejada <johnnyfran20002@gmail.com>
2022-04-04 14:41:08 -0400

    creating post models and controller

--- server/index.js
+++ server/index.js
@@ -1,13 +1,12 @@
 require('dotenv').config();
 const express = require('express')
 const usersController = require('./controllers/users')
+const postsController = require('./controllers/posts')
 const app = express()
 const port = process.env.PORT || 3000
 
-//console.log(process.env)
-
 app
-  .use('/', express.static(__dirname + '/public/'))
+  .use('/', express.static(__dirname + '/public'))
 
   .use(express.json())
   
@@ -15,6 +14,7 @@ app
     res.send('You are at the root of the API for the best class ever - ' + process.env.BEST_CLASS_EVER)
   })
   .use('/api/users', usersController)
+  .use('/api/posts', postsController)
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
