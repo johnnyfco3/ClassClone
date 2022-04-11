c101001c42ab752553e435ed68d32c04dca14084
johnnytejada <johnnyfran20002@gmail.com>
2022-04-11 14:53:49 -0400

    full crud for users

--- server/models/user.js
+++ server/models/user.js
@@ -38,30 +38,43 @@ const list = [
 
 async function get(id){
     const user = await collection.findOne({_id: new ObjectId(id)})
+    if(!user){
+        throw { statusCode: 404, message: 'User not found'}
+    }
+    return { ...user, password: undefined }
+}
+
+async function getByHandle(handle){
+    const user = await collection.findOne({handle})
+    if(!user){
+        throw { statusCode: 404, message: 'User not found'}
+    }
     return { ...user, password: undefined }
 }
 
 async function remove(id){
-    const user = await collection.findOneAndDelete({_id: ObjectId(id)})
+    const user = await collection.findOneAndDelete({_id: new ObjectId(id)})
 
     return { ...user.value, password: undefined }
 }
 
 async function update(id, newUser){
-    const index = list.findIndex(user => user.id === parseInt(id))
-    const oldUser = list[index]
-
     if(newUser.password){
         newUser.password = await bcrypt.hash(newUser.password, +process.env.SALT_ROUNDS)
     }
     
-    newUser = list[index] = { ...oldUser, ...newUser }
+    newUser = await collection.findOneAndUpdate(
+        {_id: new ObjectId(id)}, 
+        {$set: newUser},
+        {returnDocument: 'after'}
+    )
 
     return { ...newUser, password: undefined }
 }
 
 async function login(email, password){
-    const user = list.find(user => user.email === email)
+    const user = await collection.findOne({email})
+
     if(!user){
         throw { statusCode: 404, message: 'User not Found' }
     }
@@ -95,13 +108,20 @@ function seed(){
 module.exports = {
     collection,
     seed,
+    getByHandle,
     async create(user) {
         user.id = ++highestId
+
+        if(!user.handle){
+            throw { statusCode: 400, message: 'Handle is required' }
+        }
 
         user.password = await bcrypt.hash(user.password, +process.env.SALT_ROUNDS)
         console.log(user)
 
-        list.push(user)
+        const result = await collection.insertOne(user)
+        user = await get(result.insertedId)
+
         return { ...user, password: undefined }
     },
     remove,
