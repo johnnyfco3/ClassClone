cf0b976bf408d999519cde6618d63e79f48d8569
johnnytejada <johnnyfran20002@gmail.com>
2022-04-18 14:41:45 -0400

    post method working

--- client/src/models/session.ts
+++ client/src/models/session.ts
@@ -2,32 +2,28 @@ import { reactive } from "vue"
 import router from "../router"
 import * as users from "../models/user"
 import { useMessage } from "./messages"
+import { api } from "./myFetch"
 
 const session = reactive({
     user: null as users.User | null,
     destinationURL: null as string | null
 })
 
-export async function Login(handle: String, password: String) {
-    const user = users.list.find(u => u.handle === handle)
+export async function Login(email: string, password: string) {
     const messages = useMessage();
 
-    try {    
-        if(!user){
-            throw { message: 'User not found' };
-        }
-    
-        if(user.password !== password){
-            throw { message: 'Incorrect Password' }
-        }
-    
-        messages.notifications.push({
-            type: "success",
-            message: `Welcome back ${user.firstName}`
-        })
+    try {
+        const user = await api("users/login", {email, password} )
 
-        session.user = user
-        router.push(session.destinationURL ?? '/wall')
+        if(user){
+            messages.notifications.push({
+                type: "success",
+                message: `Welcome back ${user.firstName}`
+            })
+
+            session.user = user
+            router.push(session.destinationURL ?? '/wall')
+        }
         
     } catch (error: any) {
         messages.notifications.push({
