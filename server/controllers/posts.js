c8c521b8a795ebd66527fff292384c5c3d233b62
johnnyfco3 <72476506+johnnyfco3@users.noreply.github.com>
2022-05-02 19:24:13 +0000

    Deploying to node-deploy from @ johnnyfco3/ClassClone@6f51f8da18b096fca3cd9012fcc7cfd1516724da 🚀

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -1,39 +1,52 @@
-const express = require("express")
-const app = express.Router()
+const express = require('express');
+const app = express.Router();
+
 const postModel = require('../models/post');
 
-const CREATED_STATUS = 201
+const CREATED_STATUS = 201;
 
 app
-    .get('/', (req,res,next) =>{
+    .get('/', (req, res, next) => {
         postModel.getList()
-        .then(posts => res.json({ success: true, errors: [], data: posts }))
-        .catch(next)
+            .then(posts => res.json({ success: true, errors: [], data: posts }))
+            .catch(next);
     })
-    .get('/wall/:handle', (req,res,next) =>{
+    .get('/wall', (req, res, next) => {
+        postModel.getWall(req.user.handle)
+            .then(posts => res.json({ success: true, errors: [], data: posts }))
+            .catch(next);
+    })
+    .get('/wall/:handle', (req, res, next) => {
+        // if the user is not friends with the requested handle, then return an error
+
         postModel.getWall(req.params.handle)
-        .then(posts => res.json({ success: true, errors: [], data: posts }))
-        .catch(next)
+            .then(posts => res.json({ success: true, errors: [], data: posts }))
+            .catch(next);
     })
-    .get('/:id', (req,res,next)=>{
+    .get('/:id', (req, res, next) => {
         postModel.get(req.params.id)
-        .then(post => res.json({ success: true, errors: [], data: post }))
-        .catch(next)
+            .then(post => res.json({ success: true, errors: [], data: post }))
+            .catch(next);
+
     })
-    .post('/', (req,res,next)=>{
+    .post('/', (req, res, next) => {
+
+        req.body.owner = req.user.handle;
+        
         postModel.create(req.body)
-        .then(post => res.status(CREATED_STATUS).json({ success: true, errors: [], data: post }))
-        .catch(next)
+            .then(post => res.status(CREATED_STATUS).json({ success: true, errors: [], data: post }))
+            .catch(next);
     })
-    .delete('/:id', (req,res,next)=>{
+    .delete('/:id', (req, res, next) => {
         postModel.remove(req.params.id)
-        .then(post => res.send({ success: true, errors: [], data: post }))
-        .catch(next)
+            .then(post => res.json({ success: true, errors: [], data: post }))
+            .catch(next);
+
     })
-    .patch('/:id', (req,res,next)=>{
+    .patch('/:id', (req, res, next) => {
         postModel.update(req.params.id, req.body)
-        .then(post => res.send({ success: true, errors: [], data: post }))
-        .catch(next)
+            .then(post => res.json({ success: true, errors: [], data: post }))
+            .catch(next);
     })
     .post('/seed', (req, res, next) => {
         postModel.seed()
@@ -41,4 +54,6 @@ app
             .catch(next);
     })
 
-module.exports = app
+
+
+module.exports = app;
